086ac82467e9af2a28453590bce3f7f69a09e980
ylrcyi <154767954+ylrcyi@users.noreply.github.com>
2025-08-03 18:18:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -115,3 +115,8 @@ function toggleMenu() {
   const menu = document.getElementById('menu');
   menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
+
+function closeMenu() {
+  const menu = document.getElementById("menu");
+  menu.style.display = "none";
+}
